e244800b1a443a6d27220d69a851a72db341d1bd
Abhijit Chede <abhijit.chede@techverito.com>
2019-08-18 21:04:19 +0530

    Validates if all the required fields are submitted

--- app/models/daily_collection.rb
+++ app/models/daily_collection.rb
@@ -1,4 +1,11 @@
 class DailyCollection < ApplicationRecord
   belongs_to :customer
   belongs_to :user
+
+  validates :litre, presence: :true
+  validates :fat, presence: :true
+  validates :lacto, presence: :true
+  validates :snf, presence: :true
+  validates :rate, presence: :true
+  validates :total_price, presence: :true
 end
